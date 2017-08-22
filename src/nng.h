0af425b9a465080d823524c1f41dff5946d535cc
Garrett D'Amore <garrett@damore.org>
2017-08-22 11:34:59 -0700

    Actually compile the zerotier bits, more plumbing progress.
    Turns out we weren't actually building the stuff right (ordering
    errors in CMakeLists.)  We've added the lower level support for
    sending wire packets now (receive not implemented yet) using
    IPv4 UDP.  No IPv6 for physical networks stuff yet -- that would
    require opening a second socket (something that may actually be worth
    doing at one point.)

--- src/nng.h
+++ src/nng.h
@@ -632,9 +632,17 @@ struct nng_sockaddr_in {
 	uint16_t sa_port;
 	uint32_t sa_addr;
 };
+
+struct nng_sockaddr_zt {
+	uint64_t sa_nwid;
+	uint64_t sa_nodeid;
+	uint16_t sa_port;
+};
+
 typedef struct nng_sockaddr_in nng_sockaddr_in;
 typedef struct nng_sockaddr_in nng_sockaddr_udp;
 typedef struct nng_sockaddr_in nng_sockaddr_tcp;
+typedef struct nng_sockaddr_zt nng_sockaddr_zt;
 
 typedef struct nng_sockaddr {
 	union {
@@ -642,6 +650,7 @@ typedef struct nng_sockaddr {
 		nng_sockaddr_path s_path;
 		nng_sockaddr_in6  s_in6;
 		nng_sockaddr_in   s_in;
+		nng_sockaddr_zt   s_zt;
 	} s_un;
 } nng_sockaddr;
 
@@ -651,6 +660,7 @@ enum nng_sockaddr_family {
 	NNG_AF_IPC    = 2,
 	NNG_AF_INET   = 3,
 	NNG_AF_INET6  = 4,
+	NNG_AF_ZT     = 5, // ZeroTier
 };
 
 #ifdef __cplusplus
